3b8c2019567e49213908ff90176b1d6a550643c6
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-17 17:11:04 -0300

    Lesson - 5 - Acessibility

--- resources/views/livewire/edit-profile.blade.php
+++ resources/views/livewire/edit-profile.blade.php
@@ -2,8 +2,7 @@
     <h1 class="mb-4 text-2xl text-slate-700 font-semibold">Update your profile...</h1>
 
     <form wire:submit="save" class="min-w-[30rem] flex flex-col gap-6 bg-white rounded-lg shadow p-6">
-
-        <div class="flex flex-col gap-2">
+        <label class="flex flex-col gap-2">
             <h3 class="font-medium text-slate-700 text-base">Username</h3>
 
             <input
@@ -13,18 +12,22 @@
                     'border border-slate-300' => $errors->missing('form.username'),
                     'border-2 border-red-500' => $errors->has('form.username'),
                 ])
-            />
+                @error('form.username')
+                    aria-invalid="true"
+                    aria-description="{{ $message }}"
+                @enderror
+            >
 
             @error('form.username')
-                <p class="text-sm text-red-500">{{ $message }}</p>
+                <p class="text-sm text-red-500" aria-live="assertive">{{ $message }}</p>
             @enderror
-        </div>
+        </label>
 
-        <div class="flex flex-col gap-2">
+        <label class="flex flex-col gap-2">
             <h3 class="font-medium text-slate-700 text-base">Bio</h3>
 
             <textarea wire:model="form.bio" rows="4" class="px-3 py-2 border border-slate-300 rounded-lg"></textarea>
-        </div>
+        </label>
 
         <div class="flex">
             <button type="submit" class="relative w-full bg-blue-500 py-3 px-8 rounded-lg text-white font-medium disabled:cursor-not-allowed disabled:opacity-75">
@@ -46,6 +49,7 @@
         x-transition.out.opacity.duration.2000ms
         x-effect="if($wire.showSuccessIndicator) setTimeout(() => $wire.showSuccessIndicator = false, 3000)"
         class="flex justify-end pt-4"
+        aria-live="polite"
     >
         <div class="flex gap-2 items-center text-green-500 text-sm font-medium">
             Profile updated successfully
