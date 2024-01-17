512f0e11dd4edfba67d4f46e60125a1b83d98c31
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-17 16:42:51 -0300

    Lesson 4 - Extracting a Form Object

--- app/Livewire/EditProfile.php
+++ app/Livewire/EditProfile.php
@@ -2,51 +2,27 @@
 
 namespace App\Livewire;
 
-use App\Models\User;
-use Illuminate\Validation\Rule;
-use Livewire\Attributes\Validate;
+use App\Livewire\Forms\ProfileForm;
 use Livewire\Component;
 
 class EditProfile extends Component
 {
-    public User $user;
-
-    #[Validate]
-    public string $username = '';
-    public string $bio = '';
+    public ProfileForm $form;
 
     public $showSuccessIndicator = false;
 
-    public function rules()
-    {
-        return [
-            'username' => [
-                'required',
-                Rule::unique('users')->ignore($this->user),
-            ]
-        ];
-    }
-
     public function mount(): void
     {
-        $this->user = auth()->user();
-
-        $this->username = $this->user->username;
-        $this->bio = $this->user->bio;
+        $this->form->setUser(auth()->user());
     }
 
     public function save(): void
     {
-        $this->validate();
-
-        $this->user->username = $this->username;
-        $this->user->bio = $this->bio;
+        $this->form->update();
 
         sleep(1);
 
         $this->showSuccessIndicator = true;
-
-        $this->user->save();
     }
 
     public function render()
